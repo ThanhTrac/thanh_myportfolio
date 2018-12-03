c748689f03164a9fbba399a50426bdaae2828d7e
Thanh Trac Tran <tran0327@algonquinlive.com>
2018-12-03 12:09:50 -0500

    Uploading Website

---
+++ my_portfolio.js
@@ -0,0 +1,102 @@
+
+$(function(){
+	$('#dropdown1').mouseover(function(){
+		$(".overlay:not(#html)").hide();
+	});
+	$('#dropdown1').mouseout(function(){
+		$(".overlay").show();		
+	});
+	
+	$('#dropdown2').mouseover(function(){
+		$(".overlay:not(#css)").hide();
+	});		
+	$('#dropdown2').mouseout(function(){
+		$(".overlay").show();		
+	});
+	
+	$('#dropdown3').mouseover(function(){
+		$(".overlay:not(#bootstrap)").hide();
+	});		
+	$('#dropdown3').mouseout(function(){
+		$(".overlay").show();		
+	});
+	
+	$('#dropdown4').mouseover(function(){
+		$(".overlay:not(#javascript)").hide();
+	});		
+	$('#dropdown4').mouseout(function(){
+		$(".overlay").show();	
+	});
+	
+	$('#dropdown5').mouseover(function(){
+		$(".overlay:not(#angular)").hide();
+	});		
+	$('#dropdown5').mouseout(function(){
+		$(".overlay").show();		
+	});
+	
+	$('#dropdown6').mouseover(function(){
+		$(".overlay:not(#react)").hide();
+	});		
+	$('#dropdown6').mouseout(function(){
+		$(".overlay").show();		
+	});
+	
+	$('#dropdown7').mouseover(function(){
+		$(".overlay:not(#node)").hide();
+	});		
+	$('#dropdown7').mouseout(function(){
+		$(".overlay").show();		
+	});
+	
+	$('#dropdown8').mouseover(function(){
+		$(".overlay:not(#php)").hide();
+	});		
+	$('#dropdown8').mouseout(function(){
+		$(".overlay").show();		
+	});
+	
+	$('#dropdown9').mouseover(function(){
+		$(".overlay:not(#mongodb)").hide();
+	});		
+	$('#dropdown9').mouseout(function(){
+		$(".overlay").show();		
+	});
+	
+	$('#dropdown10').mouseover(function(){
+		$(".overlay:not(#django)").hide();
+	});		
+	$('#dropdown10').mouseout(function(){
+		$(".overlay").show();		
+	});
+	
+	$('#dropdown11').mouseover(function(){
+		$(".overlay:not(#drupal)").hide();
+	});		
+	$('#dropdown11').mouseout(function(){
+		$(".overlay").show();		
+	});
+	
+	$('#dropdown12').mouseover(function(){
+		$(".overlay:not(#python)").hide();
+	});		
+	$('#dropdown12').mouseout(function(){
+		$(".overlay").show();		
+	});
+	
+	$('#dropdown13').mouseover(function(){
+		$(".overlay:not(#java)").hide();
+	});		
+	$('#dropdown13').mouseout(function(){
+		$(".overlay").show();		
+	});
+	
+	$('#dropdown14').mouseover(function(){
+		$(".overlay:not(#wordpress)").hide();
+	});		
+	$('#dropdown14').mouseout(function(){
+		$(".overlay").show();		
+	});
+	
+});
+	
